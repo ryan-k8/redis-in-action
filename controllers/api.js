7c628073258e6d1e1081d80b17ccad48ba4638ae
ryan-school2020 <ryan.khursheedps@gmail.com>
2022-02-05 23:25:48 +0530

    implemented some caching with redis

---
+++ controllers/api.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+const redisClient = require("../util/redis");
+
+exports.search = async (req, res, next) => {
+  try {
+    const { searchQuery } = req.params;
+
+    const reply = await redisClient.get(searchQuery);
+
+    if (reply) {
+      return res.json(JSON.parse(reply));
+    }
+
+    const { data } = await axios.get(
+      `${process.env.API_URL}/search/${searchQuery}`
+    );
+    await redisClient.set(searchQuery, JSON.stringify(data), 60);
+
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.showDetails = async (req, res, next) => {
+  try {
+    const { showId } = req.params;
+
+    const reply = await redisClient.get(showId);
+
+    if (reply) {
+      return res.json(JSON.parse(reply));
+    }
+    const { data } = await axios.get(
+      `${process.env.API_URL}/getdetails/${showId}`
+    );
+
+    await redisClient.set(showId, JSON.stringify(data), 3600);
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.streamEp = async (req, res, next) => {
+  try {
+    console.log(req.connection);
+    const { showId, epNo } = req.params;
+
+    const reply = await redisClient.get(showId + epNo);
+
+    if (reply) {
+      return res.json(JSON.parse(reply));
+    }
+
+    const { data } = await axios.get(
+      `${process.env.API_URL}/stream/${showId}/ep/${epNo}`
+    );
+
+    const cacheTime = data.episode_exists == "false" ? 3600 * 24 : 10;
+
+    await redisClient.set(showId + epNo, JSON.stringify(data), cacheTime);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
-const isAuth = require("../middlewares/is-auth");
-
 const router = express.Router();
+const isAuth = require("../middlewares/is-auth");
+const apiController = require("../controllers/api");
 
-router.get("/", isAuth, (req, res, next) => {
-  res.status(200).json({
-    message: "seeing protected stuff",
-  });
-});
+router.get("/search/:searchQuery", isAuth, apiController.search);
+
+router.get("/show/:showId", isAuth, apiController.showDetails);
+
+/**
+ * add rate limiting here
+ */
+router.get("/stream/:showId/ep/:epNo", isAuth, apiController.streamEp);
 
 module.exports = router;
